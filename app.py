6397f70572a9955accd3425f842f0f7b759a5b93
hattiq <h.attiqurrehman2@gmail.com>
2023-03-19 03:55:48 +0500

    adding files

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
-from transformers import pipeline
+from torchvision import transforms
 import torch
+from model import OnnxModel
+from PIL import Image
+from io import BytesIO
+import base64
+
+
+def preprocess_numpy(img):
+    resize = transforms.Resize((224, 224))   #must same as here
+    crop = transforms.CenterCrop((224, 224))
+    to_tensor = transforms.ToTensor()
+    normalize = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    img = resize(img)
+    img = crop(img)
+    img = to_tensor(img)
+    img = normalize(img)
+    return img
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
@@ -7,7 +23,7 @@ def init():
     global model
     
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    model = OnnxModel('./onnx_model_name.onnx')
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
@@ -15,12 +31,14 @@ def inference(model_inputs:dict) -> dict:
     global model
 
     # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
+    prompt = model_inputs.get('input', None)
+    print(prompt[:100])
     if prompt == None:
         return {'message': "No prompt provided"}
     
-    # Run the model
-    result = model(prompt)
+    img = Image.open(BytesIO(base64.b64decode(prompt)))
+    inp = preprocess_numpy(img).unsqueeze(0)
+    res = model.infer(inp)
 
     # Return the results as a dictionary
-    return result
+    return res
--- test.py
+++ test.py
@@ -2,8 +2,19 @@
 # Run it with `python3 test.py``
 
 import requests
+from PIL import Image
+import base64
 
-model_inputs = {'prompt': 'Hello I am a [MASK] model.'}
+from io import BytesIO
+
+
+img = Image.open("../mtailor_mlops_assessment/n01667114_mud_turtle.JPEG")
+im_file = BytesIO()
+img.save(im_file, format="JPEG")
+im_bytes = im_file.getvalue()  # im_bytes: image in binary format.
+im_b64 = base64.b64encode(im_bytes)
+
+model_inputs = {'input': im_b64.decode()}
 
 res = requests.post('http://localhost:8000/', json = model_inputs)
 
